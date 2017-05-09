4d38c994b05014af8915441c990af46009fe1eb4
smori1983 <shinichiro.mori.1983@gmail.com>
2017-05-10 02:49:13 +0900

    add tbm.background.serverFactory - forgot to add

---
+++ src/js/tbm/background.serverFactory.js
@@ -0,0 +1,12 @@
+tbm.background.serverFactory = (function() {
+    var that = {};
+
+    /**
+     * @param chrome.bookmarks bookmarksApi
+     */
+    that.create = function(bookmarksApi) {
+        return tbm.background.server2(bookmarksApi);
+    };
+
+    return that;
+})();
