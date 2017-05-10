32a9f0d65e134c7890799a005840ddbfbc8f4169
smori1983 <shinichiro.mori.1983@gmail.com>
2017-05-11 02:09:17 +0900

    add log output in main/background.js

--- src/js/main/background.js
+++ src/js/main/background.js
@@ -5,21 +5,31 @@ smodules.util.console.on();
 server.start();
 
 chrome.bookmarks.onChanged.addListener(function(id, changeInfo) {
+    smodules.util.console.log('bookmark changed.');
+    smodules.util.console.log(changeInfo);
     server.crawl();
 });
 chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
+    smodules.util.console.log('bookmark created.');
+    smodules.util.console.log(bookmark);
     server.crawl();
 });
 chrome.bookmarks.onMoved.addListener(function(id, moveInfo) {
+    smodules.util.console.log('bookmark moved.');
+    smodules.util.console.log(moveInfo);
     server.crawl();
 });
 chrome.bookmarks.onRemoved.addListener(function(id, removeInfo) {
+    smodules.util.console.log('bookmark removed.');
+    smodules.util.console.log(removeInfo);
     server.crawl();
 });
 chrome.bookmarks.onImportBegan.addListener(function() {
+    smodules.util.console.log('bookmark import started.');
     server.importLock();
 });
 chrome.bookmarks.onImportEnded.addListener(function() {
+    smodules.util.console.log('bookmark import ended.');
     server.importUnlock();
     server.crawl();
 });
